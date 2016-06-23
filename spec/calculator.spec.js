b54686fdb71b36932b0fc46972d4992eb5b54214
tallurivenkat <tallurivenkat@gmail.com>
2016-06-23 14:31:29 -0400

    Initial commit

---
+++ spec/calculator.spec.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Venkat Talluri on 6/23/2016.
+ */
+describe('calculator',function(){
+    it('Should add two numbers',function(){
+        expect(add(5,4)).toBe(9);
+    })
+
+})
---
+++ src/calculator.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Venkat Talluri on 6/23/2016.
+ */
+function add(x,y){
+    return x+y;
+}
